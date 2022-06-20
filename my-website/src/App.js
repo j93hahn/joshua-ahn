8a071c8e4cfb394fe0816227db43e3826a41fc3e
Joshua Ahn <joshuajahn2001@gmail.com>
2022-06-19 20:28:10 -0500

    experimentation with JS objects in ES6

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,8 +5,18 @@ import './App.css';
  * Important commands:
  * 1. npm start - runs localhost environment
  * 2. npm run build - builds production environment
- * 3. npm run deploy - deploy the App to github pages
+ * 3. npm run deploy - deploy the App to github pages (build included)
  */
+
+class project_container { // object for project displays
+    constructor(name, topic) {
+        this.name = name;
+        this.topic = topic;
+    }
+}
+
+const MLP = new project_container("Multi-Layer Perceptron", "Machine Learning");
+const Transformer = new project_container("Transformer", "Machine Learning");
 
 function App() {
     return (
@@ -15,6 +25,10 @@ function App() {
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>
+                    {MLP.name}
+                    {Transformer.name}
                 </p>
                 <a
                 className="App-link"
