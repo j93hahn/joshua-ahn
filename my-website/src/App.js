2f18436d1b2da51466ee2b5bf1e319c5f715c7a3
Joshua Ahn <joshuajahn2001@gmail.com>
2022-06-19 19:55:40 -0500

    important commands included

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,26 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+ * Important commands:
+ * 1. npm start - runs localhost environment
+ * 2. npm run build - builds production environment
+ * 3. npm run deploy - deploy the App to github pages
+ */
+
 function App() {
     return (
         <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <p>
-                Include any specifications into this file.
-            </p>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-            </a>
-        </header>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
         </div>
     );
 }
